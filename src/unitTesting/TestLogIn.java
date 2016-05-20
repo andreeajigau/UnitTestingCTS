a7d0ed856a5e2538cf4c81b19b082e6b9a4116a0
andreeajigau <andreea.jigau@gmail.com>
2016-05-20 11:44:29 +0300

    Testare proiect

---
+++ src/unitTesting/TestLogIn.java
@@ -0,0 +1,15 @@
+package unitTesting;
+
+import junit.framework.TestCase;
+
+public class TestLogIn extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+}
